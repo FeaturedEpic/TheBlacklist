60302534220b1b96197b35dd9d5a81b4c92ec0de
FeaturedEpic <lukecarrwebdesign@googlemail.com>
2017-06-02 23:10:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+  console.log("Getting JSON");
   $.getJSON("blacklist.json", function(data){
+    console.log("[DONE] Getting JSON");
     var blacklisters = data.blacklist;
+    console.log("[DONE] Getting Blacklisters");
+    console.log(blacklisters);
     $.each(blacklisters, function(index,el){
-      alert(el.name);
+      console.log(el.name);
     });
   });
 });
